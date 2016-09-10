9cd184c38fb2eef9d540500a2cf41a043f54ce85
Andrei Neacșu <amneacsu@gmail.com>
2016-09-10 16:40:11 +0300

    avoid globals

--- src/game.js
+++ src/game.js
@@ -1,5 +1,3 @@
-let cells = [];
-
 const neighbourhood = [
   { x: -1, y: -1 },
   { x: -1, y: 0 },
@@ -12,28 +10,25 @@ const neighbourhood = [
 ];
 
 const rand = (min, max) => ~~(Math.random() * (max - min + 1)) + min;
-const spawn = cell => !find(cell) && cells.push(cell);
+const spawn = (list, cell) => !find(list, cell) && list.push(cell);
 const offset = (cell, offs) => ({ x: cell.x + offs.x, y: cell.y + offs.y });
 const area = cell => neighbourhood.map(offs => offset(cell, offs));
 
-const liveNeighbours = cell => area(cell).filter(n => find(n)).length;
-const countNeighbours = cell => cell.n = liveNeighbours(cell);
+const liveNeighbours = (list, cell) => area(cell).filter(n => find(list, n)).length;
+const countNeighbours = (cell, i, list) => cell.n = liveNeighbours(list, cell);
 const isComfy = cell => [2, 3].indexOf(cell.n) > -1;
 const isRipe = cell => cell.n === 3;
 
-const find = cell => cells.find(c => c.x === cell.x && c.y === cell.y);
+const find = (list, cell) => list.find(c => c.x === cell.x && c.y === cell.y);
 
-function freeSpace(space, cell) {
+function freeSpace(space, cell, index, list) {
   const newSpace = [];
 
   area(cell).forEach(function getSpace(pos) {
-    let cell = find(pos);
-
-    if (cell)
-      cell.n += 1;
+    let cell = find(list, pos);
 
     if (!cell) {
-      const d = space.find(ds => ds.x === pos.x && ds.y === pos.y);
+      const d = find(space, pos);
       if (d) d.n += 1;
       else newSpace.push({ x: pos.x, y: pos.y, n: 1 });
     }
@@ -42,29 +37,36 @@ function freeSpace(space, cell) {
   return space.concat(newSpace);
 }
 
-function populate(n) {
-  for (let i = cells.length; i < n; i++) {
+function populate(list, n) {
+  for (let i = list.length; i < n; i++) {
     let x = rand(0, 100);
     let y = rand(0, 100);
-    spawn({ x, y });
+    spawn(list, { x, y });
   }
 }
 
-function tick() {
-  cells.forEach(countNeighbours);
-  const survivors = cells.filter(isComfy);
-  const newCells = cells.reduce(freeSpace, []).filter(isRipe);
-  cells = survivors.concat(newCells);
+function tick(list) {
+  list.forEach(countNeighbours);
+  const survivors = list.filter(isComfy);
+  const newCells = list.reduce(freeSpace, []).filter(isRipe);
+  return survivors.concat(newCells);
 }
 
 export default function(seedSize = 1000, interval = 100) {
-  populate(seedSize);
-  setInterval(tick, interval);
+  let _grid = [];
+
+  populate(_grid, seedSize);
+
+  setInterval(function() {
+    _grid = tick(_grid);
+  }, interval);
 
   return {
-    spawn,
+    spawn(cell) {
+      spawn(_grid, cell);
+    },
     get cells() {
-      return cells;
+      return _grid;
     },
   };
 }
